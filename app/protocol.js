c9cd2faec3b13c841f7c15ba32e34fd61625d5a8
Jared Hanson <jaredhanson@gmail.com>
2017-12-16 15:43:17 -0800

    Add protocol.

---
+++ app/protocol.js
@@ -0,0 +1,6 @@
+exports = module.exports = function(logger) {
+  return require('../lib/protocol');
+}
+
+exports['@implements'] = 'http://i.bixbyjs.org/ms/protocol';
+exports['@name'] = 'gcp-pubsub';
