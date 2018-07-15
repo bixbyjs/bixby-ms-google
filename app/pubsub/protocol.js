6c3ac0dc9cd838cafee33af717429e9fb5376b9a
Jared Hanson <jaredhanson@gmail.com>
2018-07-14 18:07:28 -0700

    Remove old code.

--- app/pubsub/protocol.js
+++
@@ -1,7 +0,0 @@
-exports = module.exports = function() {
-  return require('../../lib/pubsub/protocol');
-}
-
-exports['@singleton'] = true;
-exports['@implements'] = 'http://i.bixbyjs.org/ms/protocol';
-exports['@name'] = 'gcp.pubsub';
