d8b5f16575344f50b6e5d79434c6f7442b1db279
Jared Hanson <jaredhanson@gmail.com>
2018-07-17 15:40:05 -0700

    Add debug logging.

--- app/http/push/handlers/push.js
+++ app/http/push/handlers/push.js
@@ -9,11 +9,13 @@ exports = module.exports = function(app, parse) {
     console.log(req.headers);
     
     function done(ok) {
+      console.log('done: ' + ok);
       if (!ok) { return res.status(500).end(); }
       return res.status(204).end();
     }
     
     var msg = new Message('test-linkback', data, done);
+    console.log('dispatching app...');
     app(msg);
   }
   
