c20c21f579af357c2e772700fcda6be432e669d1
Jared Hanson <jaredhanson@gmail.com>
2018-07-17 15:44:49 -0700

    Silence verbose logging.

--- app/http/push/handlers/push.js
+++ app/http/push/handlers/push.js
@@ -5,17 +5,12 @@ exports = module.exports = function(app, parse) {
   function handle(req, res, next) {
     var data = Buffer.from(req.body.message.data, 'base64')
     
-    
-    console.log(req.headers);
-    
     function done(ok) {
-      console.log('done: ' + ok);
       if (!ok) { return res.status(500).end(); }
       return res.status(204).end();
     }
     
     var msg = new Message('test-linkback', data, done);
-    console.log('dispatching app...');
     app(msg);
   }
   
